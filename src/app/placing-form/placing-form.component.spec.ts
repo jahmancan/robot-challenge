8981712f39a9edd08c9b6e43685b6b745a8cd26e
jahmancan <matyas@matyas.nl>
2025-04-05 17:32:50 +1100

    fix: tests for existing components

--- src/app/placing-form/placing-form.component.spec.ts
+++ src/app/placing-form/placing-form.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlacingFormComponent } from './placing-form.component';
+import { GRID_DIMENSION } from '../shared';
 
 describe('PlacingFormComponent', () => {
-  let component: PlacingFormComponent;
-  let fixture: ComponentFixture<PlacingFormComponent>;
+   let component: PlacingFormComponent;
+   let fixture: ComponentFixture<PlacingFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PlacingFormComponent]
-    })
-    .compileComponents();
+   beforeEach(async () => {
+      await TestBed.configureTestingModule({
+         imports: [PlacingFormComponent],
+         providers: [
+            PlacingFormComponent,
+            {
+               provide: GRID_DIMENSION,
+               useValue: 5,
+            },
+         ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(PlacingFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      fixture = TestBed.createComponent(PlacingFormComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should create', () => {
+      expect(component).toBeTruthy();
+   });
 });
--- src/app/report/report.component.spec.ts
+++ src/app/report/report.component.spec.ts
@@ -1,23 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReportComponent } from './report.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Direction } from '../shared';
 
 describe('ReportComponent', () => {
-  let component: ReportComponent;
-  let fixture: ComponentFixture<ReportComponent>;
+   let component: ReportComponent;
+   let fixture: ComponentFixture<ReportComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ReportComponent]
-    })
-    .compileComponents();
+   beforeEach(async () => {
+      await TestBed.configureTestingModule({
+         imports: [ReportComponent],
+         providers: [
+            ReportComponent,
+            {
+               provide: MAT_DIALOG_DATA,
+               useValue: { x: 0, y: 0, direction: Direction.east },
+            },
+         ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ReportComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      fixture = TestBed.createComponent(ReportComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+   it('should create', () => {
+      expect(component).toBeTruthy();
+   });
 });
